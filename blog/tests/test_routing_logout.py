5a44dd9c75995a0c2dcfd9541a7970d710b6306d
val-sytch <val.sytch@gmail.com>
2016-09-18 21:40:53 +0300

    test for routing logout page

---
+++ blog/tests/test_routing_logout.py
@@ -0,0 +1,24 @@
+import blog
+import unittest
+
+
+class RoutingLogoutTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = blog.app.test_client()
+
+    def test_logout_page_get(self):
+        response_get = self.app.get('/logout', follow_redirects=True)
+        self.assertEqual(response_get.status_code, 200)
+        self.assertIn('You were logged out', response_get.data)
+
+    def test_logout_page_other(self):
+        response_post = self.app.post('/logout')
+        response_put = self.app.put('/logout')
+        response_delete = self.app.delete('/logout')
+        self.assertEqual(response_post.status_code, 405)
+        self.assertEqual(response_put.status_code, 405)
+        self.assertEqual(response_delete.status_code, 405)
+
+if __name__ == '__main__':
+    unittest.main()
