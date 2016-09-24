8bc71a6402ca73d820e5bd0e540b5a780d8911a1
val-sytch <val.sytch@gmail.com>
2016-09-24 20:25:00 +0300

    update fabfile.py(add functions for launching pylint and coverage)

--- fabfile.py
+++ fabfile.py
@@ -1,11 +1,28 @@
 from fabric.api import local
 
 
-def run_test():
-    local('cd blog/tests/')
-    local('python -m unittest discover')
-    local('cd ../../')
+def run_test_coverage():
+    local('nosetests blog/tests/ --with-coverage --cover-package="blog"')
 
 
 def make_targz():
     local('python setup.py sdist')
+
+
+def tests_package_requir():
+    local('pip install nose')
+    local('pip install --upgrade setuptools')
+    local('pip install mock')
+
+
+def review_package_requir():
+    local('pip install coverage')
+    local('pip install pylint')
+
+
+def run_pylint_to_files():
+    local('pylint --files-output=y --disable=line-too-long blog/')
+
+
+def run_pylint_to_console():
+    local('pylint --disable=line-too-long blog/')
